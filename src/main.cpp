b03afe19b21120c7ba43265f6bdca452fa272a78
itsgosho <itsgosho2@gmail.com>
2021-08-02 10:42:29 +0300

    Added a option to detect a signal by length

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@
 unsigned long calculateHighTimeMS() {
 
     while (digitalRead(PIR_SENSOR_PIN) == LOW);
-
+    Serial.println("Sensing now...");
     unsigned long highTimeStartMS = millis();
 
     while (digitalRead(PIR_SENSOR_PIN) == HIGH);
 
     unsigned long highTimeEndMS = millis();
+    Serial.println("End sensing!");
 
     return highTimeEndMS - highTimeStartMS;
+}
+
+/**
+ * Will block until a high signal is detected, which length is equal or above the given one
+ */
+void detectBySignalLength(const unsigned long& signalLength) {
+    while (calculateHighTimeMS() < signalLength);
 }
 
 void setup() {
@@ -22,13 +30,13 @@ void setup() {
 }
 
 void loop() {
-/*    short value = digitalRead(PIR_SENSOR_PIN);
+    /*short value = digitalRead(PIR_SENSOR_PIN);
 
     Serial.println(value);*/
 
-   unsigned long highTime = calculateHighTimeMS();
+    unsigned long highTime = calculateHighTimeMS();
 
-   if(highTime > 0) {
-       Serial.println(highTime);
-   }
+    if (highTime > 0) {
+        Serial.println(highTime);
+    }
 }
